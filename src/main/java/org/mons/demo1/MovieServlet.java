6c96baf511c7463a38fb668701c7ba1f7959c98b
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-10 09:54:18 +0100

    CM 3

--- src/main/java/org/mons/demo1/MovieServlet.java
+++ src/main/java/org/mons/demo1/MovieServlet.java
@@ -7,6 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet(name="movieServlet",value = "/movies")
@@ -21,4 +25,26 @@ public class MovieServlet extends HttpServlet {
 
         request.getRequestDispatcher("movies.jsp").forward(request, response);
     }
+    private void showAllMovies(HttpServletRequest req, HttpServletResponse resp) throws SQLException, ServletException, IOException {
+
+
+        try{
+
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://db:3306/movies","root","secret");
+        PreparedStatement pst = conn.prepareStatement("select * from movies where name like = ? ");
+        pst.setString(1,"rings");
+
+        req.setAttribute("movies",movies);
+        req.getRequestDispatcher("/movies.jsp").forward(req,resp);
+
+
+    }
+
+
+
 }
