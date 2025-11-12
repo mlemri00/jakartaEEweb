d293a32c9ce1ee90a898d29a263107a71b185722
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-12 18:36:00 +0100

    CM 8

--- src/main/java/org/mons/demo1/services/MovieServiceDBImpl.java
+++ src/main/java/org/mons/demo1/services/MovieServiceDBImpl.java
@@ -1,4 +1,49 @@
 package org.mons.demo1.services;
 
-public class MovieServiceDBImpl {
+import org.mons.demo1.models.Movie;
+import org.mons.demo1.util.jdbcConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MovieServiceDBImpl implements MovieService{
+
+    private List<Movie> movies;
+    private Connection connection = jdbcConnector.getConnection();
+
+    private void retrieveAllMovies(){
+        PreparedStatement pst;
+        ResultSet result;
+        try {
+             pst = connection.prepareStatement("SELECT * FROM movies ");
+             result = pst.executeQuery();
+
+        } catch (SQLException sqlException){
+            System.out.println("No se pudo ejecutar la conexión con la bases de datos");
+        }
+
+
+    }
+    @Override
+    public List<Movie> getMovies() {
+        return List.of();
+    }
+
+    @Override
+    public Movie getById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean addMovie(Movie movie) {
+        return false;
+    }
+
+    @Override
+    public Movie deleteMovieById(int id) {
+        return null;
+    }
 }
