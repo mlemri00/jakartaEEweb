93f2cf46f58c28eeb4b67fd77d9c148272e4cfed
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-15 14:27:31 +0100

    CM 19

--- src/main/java/org/mons/demo1/services/MovieServiceImpl.java
+++ src/main/java/org/mons/demo1/services/MovieServiceImpl.java
@@ -1,4 +1,33 @@
 package org.mons.demo1.services;
 
-public class MovieServiceImpl {
+import org.mons.demo1.DAO.MovieORMImpl;
+import org.mons.demo1.DAO.MovieService;
+import org.mons.demo1.models.Movie;
+
+import java.util.List;
+
+public class MovieServiceImpl extends MovieService{
+
+    MovieService dataRaw = new MovieORMImpl();
+
+
+    @Override
+    public List<Movie> getMovies() {
+        return dataRaw.getMovies();
+    }
+
+    @Override
+    public Movie getById(int id) {
+        return dataRaw.getById(id);
+    }
+
+    @Override
+    public boolean addMovie(Movie movie) {
+        return dataRaw.addMovie(movie);
+    }
+
+    @Override
+    public Movie deleteMovieById(int id) {
+        return dataRaw.deleteMovieById(id);
+    }
 }
